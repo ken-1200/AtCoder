83013da819bdd98105208e697798daf3a4a4c40e
ken-1200 <kensuke19971022@gmail.com>
2021-10-27 02:05:39 +0900

    second commit

--- 20211026.py
+++ 20211026.py
@@ -12,16 +12,13 @@ def main():
     0≤Y≤9
     X と Y は整数
     """
-    x = str(input("1≤X≤15の整数を入力して下さい"))
-    # if x < 1 or x > 15:
-    #     return print("正しい文字列を入力してください")
-
-    idx = str(x).find(".")
-    s = x[:idx]
-    print(idx)
-
-    y = int(input("0≤Y≤9の整数を入力して下さい"))
-    if y < 0 or y > 9:
+    xy = str(input("「1≤X≤15」の整数と「0≤Y≤9」の整数を「X.Y」の形式で入力して下さい"))
+    idx = str(xy).find(".")
+    x = xy[:idx]
+    y = xy[idx + 1 :]
+    if int(x) < 1 or int(x) > 15:
+        return print("正しい文字列を入力してください")
+    if int(y) < 0 or int(y) > 9:
         return print("正しい文字列を入力してください")
 
     if int(y) >= 7:
